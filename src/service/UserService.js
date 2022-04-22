84a7026f8a82003b8c3526a1d8edc9e18378d4f1
Pankaj <Pankaj>
2022-04-22 08:41:15 +0530

    final changes

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,6 +1,6 @@
 import axios  from 'axios';
 
-const DATA_TRACKER_API_URL= 'http://localhost:8080/api/v1/state';
+const DATA_TRACKER_API_URL= 'http://covidtestapp1-env.eba-mn6ba3au.us-east-1.elasticbeanstalk.com/api/v1';
 
 
 export const  UserService = async () =>{
